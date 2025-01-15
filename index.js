ce84fa6e300c3c06c191d3112e56206bdd5e5302
geekAshish <139535109+geekAshish@users.noreply.github.com>
2025-01-15 21:31:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,64 @@ const sum = add(4, 5)
 const sub = subtract(41, 5)
 console.log(sum, sub);
 
+
+
+
+// console.log("Getting Exciting");
+
+// function add(a, b) {
+//   let sum = a + b;
+//   return sum;
+// }
+
+// const sum = require("./math.js");
+// const value = sum(3, 4);
+// console.log(value);
+
+// fs module
+// const fs = require("node:fs");
+
+// reading file sync way - readFileSync
+// const fileContentsBuffer = fs.readFileSync("./file.txt");
+// console.log(fileContentsBuffer, "binary data");
+
+// async way of reading file
+// const fileContents = fs.readFileSync("./file.txt", "utf-8");
+// console.log(fileContents);
+
+// fs.readFile("./file.txt", "utf-8", (error, data) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// fs.writeFileSync("./great.txt", "This is cool");
+
+// fs.writeFile("./great.txt", "\nThis is cool again", { flag: "a" }, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("File written successfully!!!");
+//   }
+// });
+
+const fs = require("node:fs");
+
+const readableStream = fs.createReadStream("./file.txt", {
+  encoding: "utf-8",
+  highWaterMark: 1,
+});
+
+const writeableStream = fs.createWriteStream("./file2.txt", {
+  encoding: "utf-8",
+});
+
+// readableStream.on("data", (chunk) => {
+//   console.log(chunk);
+//   writeableStream.write(chunk);
+// });
+
+// alternative way
+readableStream.pipe(writeableStream);
